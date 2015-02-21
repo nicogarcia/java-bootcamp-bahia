790291d55a5b30c8a6338dc81889f7d4c356c294
nicogarcia <nicoalbo90@gmail.com>
2015-02-21 03:26:35 -0300

    Exercise 2

---
+++ src/Topic_3/main/WordWrapper.java
@@ -0,0 +1,61 @@
+package Topic_3.main;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class WordWrapper {
+
+	public List<String> wordwrapText(String text, int rowLength) {
+		List<String> result = new LinkedList<String>();
+
+		while (text.length() > rowLength) {
+			String currentRow = text.substring(0, Math.min(rowLength, text.length()));
+			String nextRow = text.substring(rowLength, Math.min(2 * rowLength, text.length()));
+
+			int rowLastSpacePos = currentRow.lastIndexOf(' ');
+			int nextRowFirstSpacePos = nextRow.indexOf(' ');
+
+			if (nextRowFirstSpacePos == 0) {
+				// Current row has complete words
+				// Add row to list
+				result.add(currentRow.trim());
+
+				// Cut text from next word
+				text = text.substring(nextRowFirstSpacePos + rowLength + 1, text.length());
+
+				continue;
+			}
+
+			// Row doesn't have complete words
+
+			if (rowLastSpacePos != -1) {
+				// Row has spaces
+
+				// Get the last-incomplete-word length
+				int incompleteWord = (currentRow.length() - 1 - rowLastSpacePos) + nextRowFirstSpacePos;
+
+				if (incompleteWord <= rowLength) {
+					// Can fit last incomplete word in next row
+					// Add current row without it
+					result.add(currentRow.substring(0, rowLastSpacePos).trim());
+
+					// Cut text from the beginning of last incomplete word
+					text = text.substring(rowLastSpacePos + 1, text.length());
+
+					continue;
+				}
+			}
+
+			// Row has no spaces or last incomplete word doesn't fit in next row
+			// Add it to the list
+			result.add(currentRow);
+
+			// Cut text from row length
+			text = text.substring(Math.min(rowLength, text.length()));
+		}
+
+		result.add(text.trim());
+
+		return result;
+	}
+}
---
+++ src/Topic_3/tests/WordWrapperTest.java
@@ -0,0 +1,75 @@
+package Topic_3.tests;
+
+import Topic_3.main.WordWrapper;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class WordWrapperTest {
+
+	@Test
+	public void wordwrapText_Bigger_Row_Than_Text_Return_Ok() {
+		List<String> wordWrapped = new WordWrapper().wordwrapText("Hola", 60);
+
+		List<String> result = Arrays.asList("Hola");
+
+		assertEquals(result.toString(), wordWrapped.toString());
+	}
+
+	@Test
+	public void wordwrapText_No_Spaces_Return_Ok() {
+		List<String> wordWrapped = new WordWrapper().wordwrapText("Holaatodoscomolesva", 7);
+
+		List<String> result = Arrays.asList("Holaato", "doscomo", "lesva");
+
+		assertEquals(result.toString(), wordWrapped.toString());
+	}
+
+	@Test
+	public void wordwrapText_With_Spaces_Return_Ok() {
+		List<String> wordWrapped = new WordWrapper().wordwrapText("Hola a todos como les va", 7);
+
+		List<String> result = Arrays.asList("Hola a", "todos", "como", "les va");
+
+		assertEquals(result.toString(), wordWrapped.toString());
+	}
+
+	@Test
+	public void wordwrapText_With_Spaces_Long_Word_Return_Ok() {
+		List<String> wordWrapped = new WordWrapper().wordwrapText("Holaaaaa a todos comooooo les va", 7);
+
+		List<String> result = Arrays.asList("Holaaaa", "a a", "todos", "comoooo", "o les", "va");
+
+		assertEquals(result.toString(), wordWrapped.toString());
+	}
+
+	@Test
+	public void wordwrapText_Hello_World_Return_Ok() {
+		List<String> wordWrapped = new WordWrapper().wordwrapText("Hello Word!", 7);
+
+		List<String> result = Arrays.asList("Hello", "Word!");
+
+		assertEquals(result.toString(), wordWrapped.toString());
+	}
+
+	@Test
+	public void wordwrapText_a_b_c_d_e_f_Return_Ok() {
+		List<String> wordWrapped = new WordWrapper().wordwrapText("a b c d e f", 3);
+
+		List<String> result = Arrays.asList("a b", "c d", "e f");
+
+		assertEquals(result.toString(), wordWrapped.toString());
+	}
+
+	@Test
+	public void wordwrapText_Exellent_Return_Ok() {
+		List<String> wordWrapped = new WordWrapper().wordwrapText("Excellent", 5);
+
+		List<String> result = Arrays.asList("Excel", "lent");
+
+		assertEquals(result.toString(), wordWrapped.toString());
+	}
+}
