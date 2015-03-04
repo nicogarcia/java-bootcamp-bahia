6d6e97d355dd283b4b02f451599b9c50f104e0c9
nicogarcia <nicoalbo90@gmail.com>
2015-03-03 22:10:00 -0300

    Bugfix

--- src/Topic_6/src/main/java/com/globant/app/actuator/controllers/AttendeeController.java
+++ src/Topic_6/src/main/java/com/globant/app/actuator/controllers/AttendeeController.java
@@ -5,6 +5,7 @@ import com.globant.app.actuator.services.IAttendeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
@@ -16,7 +17,7 @@ public class AttendeeController {
 	@Autowired
 	private IAttendeeService attendeeService;
 
-	@RequestMapping("")
+	@RequestMapping(value = "", method = RequestMethod.GET)
 	@ResponseBody
 	public Collection<Attendee> getAll() {
 		return attendeeService.all();
--- src/Topic_6/src/main/java/com/globant/app/actuator/controllers/RoomController.java
+++ src/Topic_6/src/main/java/com/globant/app/actuator/controllers/RoomController.java
@@ -5,6 +5,7 @@ import com.globant.app.actuator.services.IRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
@@ -16,7 +17,7 @@ public class RoomController  {
 	@Autowired
 	private IRoomService roomService;
 
-	@RequestMapping("")
+	@RequestMapping(value = "", method = RequestMethod.GET)
 	@ResponseBody
 	public Collection<Room> getAllRooms() {
 		return roomService.all();
